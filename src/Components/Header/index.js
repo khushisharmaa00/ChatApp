26dd302a7829e8cafb6fb98315e2b14f54cde4ee
Khushiiii <khushisharmaa00@gmail.com>
2024-12-25 03:05:20 +0530

    storing the data in firebase

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Layout, Typography } from "antd";
 import "./index.css";
-import { Link } from "react-router-dom";
 
 const { Header } = Layout;
 const { Title } = Typography;
@@ -9,7 +8,6 @@ const { Title } = Typography;
 const AppHeader = () => {
   return (
     <Header className="header">
-      <Link to="/register">Sign Up</Link>
       <Title level={3} className="header-title">
         Chat App
       </Title>
